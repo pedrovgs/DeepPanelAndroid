35a201f5ca2c0662754813f44d8a3644519cb2ca
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2020-06-22 17:32:37 +0200

    First time we get the prediction working

--- app/src/main/java/com/github/pedrovgs/deeppanel/MainActivity.kt
+++ app/src/main/java/com/github/pedrovgs/deeppanel/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.github.pedrovgs.deeppanel
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        DeepPanel().initialize(this)
+        val deepPanel = DeepPanel()
+        deepPanel.initialize(this)
+        val bitmapSamplePage = resources.getDrawable(R.drawable.sample_page_3, null).toBitmap()
+        val result = deepPanel.extractPanels(bitmapSamplePage)
+        image.setImageBitmap(result.imageInput)
+        prediction.setImageBitmap(result.resizedImage)
+        val predictedBitmap = createBitmapFromPrediction(result.prediction)
+        mask.setImageBitmap(predictedBitmap)
+    }
+
+    private fun createBitmapFromPrediction(prediction: Array<Array<Int>>): Bitmap {
+        val imageSize = 224
+        val bitmap = Bitmap.createBitmap(imageSize, imageSize, Bitmap.Config.ARGB_8888)
+        for (x in 0 until imageSize) {
+            for (y in 0 until imageSize) {
+                val color = when (prediction[x][y]) {
+                        0 -> Color.BLUE
+                        1 -> Color.RED
+                        else -> Color.GREEN
+                    }
+                bitmap.setPixel(x, y, color)
+            }
+        }
+        return bitmap
     }
 }
--- deeppanel/src/main/java/com/github/pedrovgs/deeppanel/DeepPanel.kt
+++ deeppanel/src/main/java/com/github/pedrovgs/deeppanel/DeepPanel.kt
@@ -1,17 +1,53 @@
 package com.github.pedrovgs.deeppanel
 
 import android.content.Context
+import android.graphics.*
+import org.tensorflow.lite.Interpreter
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
-import org.tensorflow.lite.Interpreter
+
 
 class DeepPanel {
+    companion object {
+        private const val inputImageWidth = 224
+        private const val inputImageHeight = 224
+    }
+
     private lateinit var interpreter: Interpreter
 
     fun initialize(context: Context) {
         val model = loadModel(context)
         interpreter = Interpreter(model)
     }
+
+    fun extractPanels(bitmap: Bitmap): PredictionResult {
+        val resizedImage = resizeInput(bitmap)
+        val modelInput = convertBitmapToByteBuffer(resizedImage)
+        val prediction =
+            Array(1) { Array(inputImageWidth) { Array(inputImageHeight) { FloatArray(3) } } }
+        interpreter.run(modelInput, prediction)
+        val labeledPrediction = transformPredictionIntoLabels(prediction[0])
+        return PredictionResult(bitmap, resizedImage, labeledPrediction)
+    }
+
+    private fun resizeInput(b: Bitmap): Bitmap {
+        val reqWidth = inputImageWidth.toFloat()
+        val reqHeight = inputImageHeight.toFloat()
+        val m = Matrix()
+        m.setRectToRect(
+            RectF(0f, 0f, b.width.toFloat(), b.height.toFloat()),
+            RectF(0f, 0f, reqWidth, reqHeight),
+            Matrix.ScaleToFit.CENTER
+        )
+        val blackBackgroundBitmap = Bitmap.createBitmap(reqWidth.toInt(), reqHeight.toInt(), Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(blackBackgroundBitmap)
+        val paint = Paint()
+        paint.color = Color.BLACK
+        canvas.drawRect(0f, 0f, reqWidth, reqHeight, paint)
+        canvas.drawBitmap(b, m, Paint())
+        return blackBackgroundBitmap
+    }
+
 
     private fun loadModel(context: Context): ByteBuffer {
         val inputStream = context.resources.openRawResource(R.raw.model)
@@ -21,4 +57,58 @@ class DeepPanel {
         byteBuffer.put(rawModelBytes)
         return byteBuffer
     }
+
+    private fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
+        val floatTypeSizeInBytes = 4
+        val numberOfChannels = 3
+        val modelInputSize =
+            floatTypeSizeInBytes * inputImageWidth * inputImageHeight * numberOfChannels
+        val imgData = ByteBuffer.allocateDirect(modelInputSize)
+        for (i in 0 until 224) {
+            for (j in 0 until 224) {
+                val pixelInfo: Int = bitmap.getPixel(i, j)
+                val normalizedRedChannel = Color.red(pixelInfo) / 255f
+                print("[$normalizedRedChannel,")
+                imgData.putFloat(normalizedRedChannel)
+                val normalizedGreenChannel = Color.green(pixelInfo) / 255f
+                print("$normalizedGreenChannel,")
+                imgData.putFloat(normalizedGreenChannel)
+                val normalizedBlueChannel = Color.blue(pixelInfo) / 255f
+                print("$normalizedBlueChannel],")
+                imgData.putFloat(normalizedBlueChannel)
+            }
+            println()
+        }
+        imgData.rewind()
+        return imgData
+    }
+
+    private fun transformPredictionIntoLabels(prediction: Array<Array<FloatArray>>): Array<Array<Int>> {
+        val labeledPrediction = Array(224) { Array(224) { 0 } }
+        for (i in 0 until 224) {
+            for (j in 0 until 224) {
+                val pixel = prediction[i][j]
+                val background = pixel[0]
+                val border = pixel[1]
+                val content = pixel[2]
+                if (background >= content && background >= border) {
+                    labeledPrediction[i][j] = 0
+                } else if (border >= background && border >= content) {
+                    labeledPrediction[i][j] = 1
+                } else if (content >= background && content >= border) {
+                    labeledPrediction[i][j] = 2
+                }
+            }
+        }
+        return labeledPrediction
+    }
+
 }
+
+typealias Prediction = Array<Array<Int>>
+
+data class PredictionResult(
+    val imageInput: Bitmap,
+    val resizedImage: Bitmap,
+    val prediction: Prediction
+)
