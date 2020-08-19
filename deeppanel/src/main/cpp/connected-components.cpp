17ee6960728215a74f7f94af2f6fccbba74995f7
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2020-08-19 19:23:00 +0200

    Recover the original recursive version

--- deeppanel/src/main/cpp/connected-components.cpp
+++ deeppanel/src/main/cpp/connected-components.cpp
@@ -1,5 +1,10 @@
+const int dx[] = {+1, 0, -1, 0};
+const int dy[] = {0, +1, 0, -1};
+
 void depth_first_search(int **connected_components, int x, int y, int current_label, int **matrix,
                         int width, int height) {
+    if (x < 0 || x == width) return;
+    if (y < 0 || y == height) return;
     int current_item = matrix[x][y];
     if (connected_components[x][y] || current_item > 1)
         return; // already labeled
@@ -7,25 +12,8 @@ void depth_first_search(int **connected_components, int x, int y, int current_la
     // mark the current cell
     connected_components[x][y] = current_label;
 
-    if (x < width)
-        depth_first_search(connected_components,
-                           x + 1,
-                           y,
-                           current_label, matrix, width, height);
-    if (x > 0)
-        depth_first_search(connected_components,
-                           x - 1,
-                           y,
-                           current_label, matrix, width, height);
-    if (y < height)
-        depth_first_search(connected_components,
-                           x,
-                           y + 1,
-                           current_label, matrix, width, height);
-    if (y > 0)
-        depth_first_search(connected_components,
-                           x,
-                           y - 1,
+    for (int direction = 0; direction < 4; ++direction)
+        depth_first_search(connected_components, x + dx[direction], y + dy[direction],
                            current_label, matrix, width, height);
 }
 
@@ -34,9 +22,9 @@ int **find_components(int **matrix, int width, int height) {
     connected_components = new int *[height];
     int initial_label = 1;
     for (int i = 0; i < width; ++i) {
-        if (connected_components[i] == NULL) {
-            connected_components[i] = new int[width];
-        }
+        connected_components[i] = new int[width];
+    }
+    for (int i = 0; i < width; ++i) {
         for (int j = 0; j < height; ++j) {
             if (!connected_components[i][j] && matrix[i][j]) {
                 depth_first_search(
